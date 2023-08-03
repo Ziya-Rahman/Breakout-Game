ec54199db243480c70520141783280b05f729a67
Ziya Ur Rahman <ziya.rahman.zr13@gmail.com>
2023-08-04 01:42:46 +0530

    Made changes for mobile compatability

--- index.js
+++ index.js
@@ -1,26 +1,46 @@
-let canvas = document.getElementById('game'),
-    ctx = canvas.getContext('2d'),
-    ballRadius = 9,
-    x = canvas.clientWidth / (Math.floor(Math.random() * Math.random() * 10) + 3),
-    y = canvas.clientHeight - 40,
-    dx = 2,
-    dy = -2;
+// Get the canvas element using its ID
+const canvas = document.getElementById('game');
+const ctx = canvas.getContext('2d');
 
-let paddleHeight = canvas.clientHeight / 20,
-    paddleWidth = canvas.clientWidth / 12;
+// Set the canvas width and height to match the screen size
+let canvasWidth = window.innerWidth;
+let canvasHeight = window.innerHeight;
+canvas.width = canvasWidth;
+canvas.height = canvasHeight;
+
+// Ball and paddle variables
+const ballRadius = canvasWidth / 75;
+let x = canvasWidth / 40;
+let y = canvasHeight - 40;
+let dx = canvasWidth / 150;
+let dy = -canvasWidth / 150;
+
+let paddleHeight, paddleWidth, rowCount, columnCount,
+    leftOffset = canvasWidth * 0.05;
+if(canvasHeight>canvasWidth){
+    //mobile
+    paddleHeight = canvasHeight / (canvasWidth * 0.3);
+    paddleWidth = canvasWidth / 4;
+    rowCount = Math.floor(canvasHeight / (canvasHeight * 0.04));
+    columnCount = Math.floor(canvasWidth / ((canvasWidth) * 0.25));
+}else{
+    paddleHeight = canvasWidth / (canvasHeight * 0.1);
+    paddleWidth = canvasHeight / 10;
+    rowCount = Math.floor(canvasHeight - (canvasWidth * 2));
+    columnCount = Math.floor(canvasHeight / canvasWidth);
+}
+
 
 // Paddle start position
-let paddleX = (canvas.clientWidth - paddleWidth) / 2;
+let paddleX = (canvasWidth - paddleWidth) / 2;
 
-// Bricks
-let rowCount = 5,
-    columnCount = 9,
-    brickWidth = 54,
-    brickHeight = 18,
-    brickPadding = 12,
-    topOffset = 40,
-    leftOffset = 33,
-    score = 0;
+
+let brickWidth = paddleWidth * .75;
+let brickHeight = paddleHeight * 1.2
+let brickPadding = paddleWidth / paddleHeight;
+let topOffset = paddleHeight * 6;
+
+let score = 0;
 
 // Bricks array
 let bricks = [];
@@ -32,21 +52,28 @@ for (let c = 0; c < columnCount; c++) {
     }
 }
 
-// Mouse moving eventListener and function
-document.addEventListener("pointermove", pointerMoveHandler, false);
+// Event listener for both desktop and mobile
+function movePaddle(event) {
+    let relativeX;
+    if (event.type === 'mousemove') {
+        relativeX = event.clientX;
+    } else if (event.type === 'touchmove') {
+        relativeX = event.touches[0].clientX;
+    }
 
-// Move paddle with mouse
-function pointerMoveHandler(e) {
-    var relativeX = e.clientX - canvas.offsetLeft;
-    if (relativeX > 0 && relativeX < canvas.clientWidth) {
+    if (relativeX > 0 && relativeX < canvasWidth) {
         paddleX = relativeX - paddleWidth / 2;
     }
 }
 
+// Add event listeners for both desktop and mobile
+document.addEventListener('mousemove', movePaddle, false);
+document.addEventListener('touchmove', movePaddle, false);
+
 // Draw paddle
 function drawPaddle() {
     ctx.beginPath();
-    ctx.roundRect(paddleX, canvas.clientHeight - paddleHeight, paddleWidth, paddleHeight, 30);
+    ctx.rect(paddleX, canvasHeight - paddleHeight, paddleWidth, paddleHeight);
     ctx.fillStyle = '#ffffff';
     ctx.fill();
     ctx.closePath();
@@ -71,7 +98,7 @@ function drawBricks() {
                 bricks[c][r].x = brickX;
                 bricks[c][r].y = brickY;
                 ctx.beginPath();
-                ctx.roundRect(brickX, brickY, brickWidth, brickHeight, 30);
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
                 ctx.fillStyle = '#ffffff';
                 ctx.fill();
                 ctx.closePath();
@@ -110,7 +137,7 @@ function hitDetection() {
 
 // Main function
 function init() {
-    ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
     trackScore();
     drawBricks();
     drawBall();
@@ -118,14 +145,14 @@ function init() {
     hitDetection();
 
     // Detect left and right walls
-    if (x + dx > canvas.clientWidth - ballRadius || x + dx < ballRadius) {
+    if (x + dx > canvasWidth - ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
 
     // Detect top wall
     if (y + dy < ballRadius) {
         dy = -dy;
-    } else if (y + dy > canvas.clientHeight - ballRadius) {
+    } else if (y + dy > canvasHeight - ballRadius) {
         // Detect paddle hits
         if (x > paddleX && x < paddleX + paddleWidth) {
             dy = -dy;
@@ -137,7 +164,7 @@ function init() {
     }
 
     // Bottom wall
-    if (y + dy > canvas.clientHeight - ballRadius || y + dy < ballRadius) {
+    if (y + dy > canvasHeight - ballRadius || y + dy < ballRadius) {
         dy = -dy;
     }
 
@@ -146,4 +173,25 @@ function init() {
     y += dy;
 }
 
+// Listen for window resize events to adjust canvas size
+window.addEventListener('resize', function () {
+    canvasWidth = window.innerWidth;
+    canvasHeight = window.innerHeight;
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
+    paddleWidth = canvasWidth / 12;
+    paddleX = (canvasWidth - paddleWidth) / 2;
+    paddleHeight = canvasHeight / 20;
+    ballRadius = canvasWidth / 75;
+    
+    rowCount = Math.floor(canvasWidth / 100); // Adjust the value to fit your preference
+    columnCount = Math.floor(canvasWidth / 75); // Adjust the value to fit your preference
+    brickWidth = (canvasWidth - (columnCount + 1) * canvasWidth / 75) / columnCount;
+    brickHeight = canvasHeight / 30;
+    brickPadding = canvasWidth / 75;
+    topOffset = canvasHeight / 15;
+    leftOffset = (canvasWidth - (brickWidth + brickPadding) * columnCount) / 2;
+});
+
+// Start the game loop
 setInterval(init, 10);
